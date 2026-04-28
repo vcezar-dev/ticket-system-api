dfc5991f3fcfae6f4c132f5c6b04bb3bb18339aa
“vcezar-dev” <vcezaroliveiraa@gmail.com>
2026-04-28 13:49:26 -0300

    fix(ci): use correct node version and adjust e2e rate limit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,7 +15,7 @@ import { Throttle } from '@nestjs/throttler';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Throttle({ default: { ttl: 60000, limit: 10 } })
+  @Throttle({ default: { ttl: 60000, limit: 20 } })
   @ApiSignInAuth()
   @Post('login')
   login(@Body() loginDto: LoginDto) {
